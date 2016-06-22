a9bc0e0cc26275e0b93a010a1c9f88eeb1c4dc75
lixiaojin123 <20141105062l@FanydeiMac13.local>
2016-06-22 18:35:49 +0800

    计算器第一次
    首先打开Xcode，创建一个项目，点击iOS，选择Application再选择Single view
    Application,设置ID和类型，把型号设置为iPhone，在view视图中添加text和button按钮，长按control键将text
    和按钮都添加到viewController中，起名为x和y与z，在方法中定义一个临时变量temp，赋值为0，然后将text强制转换为int型，并
    将x和y相加的值放在临时变量temp中，然后再将temp转换为text类型

--- jisuan/ViewController.swift
+++ jisuan/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var x: UITextField!
+    @IBOutlet weak var y: UITextField!
+    @IBOutlet weak var z: UITextField!
+    @IBAction func add(sender: AnyObject) {
+        var temp=0
+        temp=(Int)(x.text!)!+(Int)(y.text!)!
+        z.text="\(temp)"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
